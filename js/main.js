c2fde19d612e0e0cb3174615d1fc8a0c7835a998
Vladimir <vladimir96ks@gmail.com>
2023-05-05 10:45:46 +0200

    test commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import SmoothScroll from './smoothScroll.js'
+
+(function () {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  let TOGGLE_MENU = false;
+
+  const queryString = window.location.search
+  const urlParams = new URLSearchParams(queryString)
+
+
+})()
